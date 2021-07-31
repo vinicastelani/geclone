18ae9fd61a4733d95f250a65fdc3a4426af4b76f
Vinícius Castelani dos Santos <visacastelani@gmail.com>
2021-07-31 17:31:05 -0300

    feat: initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <top-navigation-bar></top-navigation-bar>
+    <div class="container">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ducimus
+      rerum quod dolorum libero sapiente? Aperiam facere id minus! Inventore
+      neque tenetur excepturi quidem tempore sapiente, beatae asperiores
+      provident. Pariatur.
+    </div>
+  </div>
+</template>
+
+<script>
+import TopNavigationBar from "@/components/TopNavigationBar.vue";
+
+export default {
+  name: "homepage",
+  components: { TopNavigationBar }
+};
+</script>
+
+<style></style>
